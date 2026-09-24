28fae57bff33f048da81a14a8744328e607aa795
Arjun korde <arjunkorde2004@gmail.com>
2026-09-24 12:19:50 +0530

    add service layer

---
+++ Services/UserService.cs
@@ -0,0 +1,13 @@
+using server.Data;
+using server.DTOs;
+using server.Models;
+
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Services;
+
+public class UserService
+{
+
+}
